71feca4c5bda5e0b77563550c518bf3e732d6183
Andrey Kashapov <kashapovandrey@gmail.com>
2018-08-01 13:55:38 +0300

    make API call to food2fork.com

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,20 @@
-// Global app controller
-import num from './test.js';
-const x = 23;
+import axios from 'axios';
 
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+// API Key: 274516562a23b68f84740042ed4a7c16
+// http://food2fork.com/api/search
+
+async function getResults(query) {
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const key = '274516562a23b68f84740042ed4a7c16';
+
+  try {
+    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes
+  } catch (error) {
+    alert(error);
+  }
+
+  console.log(recipes);
+}
+
+getResults('pizza');
