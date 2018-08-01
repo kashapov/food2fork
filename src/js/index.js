0cf81ba93371bd7f248dc32642eaec751805f7d6
Andrey Kashapov <kashapovandrey@gmail.com>
2018-08-01 14:30:50 +0300

    building Search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,6 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-// API Key: 274516562a23b68f84740042ed4a7c16
-// http://food2fork.com/api/search
+const search = new Search('pizza');
+console.log(search);
 
-async function getResults(query) {
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const key = '274516562a23b68f84740042ed4a7c16';
-
-  try {
-    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes
-  } catch (error) {
-    alert(error);
-  }
-
-  console.log(recipes);
-}
-
-getResults('pizza');
+search.getResults();
