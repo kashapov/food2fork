0fcbc6592d048e7afa5cd04a1d2d260d2d0f4da2
Andrey Kashapov <kashapovandrey@gmail.com>
2018-08-05 19:43:16 +0300

    building Recipe model

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { key, proxy } from '../config';
 
 export default class Search {
   constructor(query) {
@@ -6,9 +7,6 @@ export default class Search {
   }
 
   async getResults() {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '274516562a23b68f84740042ed4a7c16';
-  
     try {
       const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
       this.result = res.data.recipes;
